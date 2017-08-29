6a3c5d60aa602a90f872b49bbecf45537b7f09bf
Alice Ferrazzi <alice.ferrazzi@gmail.com>
2017-08-29 19:08:46 +0900

    setup.py: updated setup file

--- setup.py
+++ setup.py
@@ -3,10 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name='elivepatch_server',
     version='0.01',
-    description='Patching differents gentoo distributions',
-    url='https://gentoo.org',
+    description='Distributed elivepatch server API',
+    url='https://wiki.gentoo.org/wiki/User:Aliceinwire/elivepatch, ' + \
+        'https://github.com/aliceinwire/elivepatch-server',
     author='Alice Ferrazzi',
     author_email='alice.ferrazzi@gmail.com',
     license='GNU GPLv2+',
     packages=find_packages(),
+    scripts=['elivepatch-server'],
 )
